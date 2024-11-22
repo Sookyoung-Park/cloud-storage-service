d1346b5e346d0794dcd7a56f62fca947273e4cff
Soo <parksk971031@gmail.com>
2024-11-22 03:45:40 -0500

    delete comment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,28 +9,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // colors: {
-      //   brand: {
-      //     DEFAULT: '#FA7275',
-      //     100: '#EA6365',
-      //   },
-      //   red: '#FF7474',
-      //   error: '#b80000',
-      //   green: '#3DD9B3',
-      //   blue: '#56B8FF',
-      //   pink: '#EEA8FD',
-      //   orange: '#F9AB72',
-      //   light: {
-      //     100: '#333F4E',
-      //     200: '#A3B2C7',
-      //     300: '#F2F5F9',
-      //     400: '#F2F4F8',
-      //   },
-      //   dark: {
-      //     100: '#04050C',
-      //     200: '#131524',
-      //   },
-      // },
       colors: {
         brand:{
           DEFAULT: "#FA7275",
