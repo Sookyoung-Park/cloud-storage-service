593e32bf684e7166bb708802fe3c5af24ff9f21e
Soo <parksk971031@gmail.com>
2024-11-22 23:26:06 -0500

    implement appwrite config.js

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,11 @@
+// import all the .env variables
+export const appwriteConfig = {
+    endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+    databaseId:process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
+    userCollectionId:process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+    fileCollectionId:process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+    bucketId:process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+    // api key on appwrite
+    secretKey:process.env.NEXT_APPWRITE_SECRET!,
+}
