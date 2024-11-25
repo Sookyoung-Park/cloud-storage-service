c5ea7bb7e357b760076a5f0ce42438838306e16f
Soo <parksk971031@gmail.com>
2024-11-25 16:01:51 -0500

    ongoing

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -13,22 +13,23 @@ const Sidebar = () => {
   return (
     <section className="sidebar gap-4">
       <nav className="sidebar-nav">
-        <Link href='/'>
+        {/* <Link href='/'>
           <Image 
             src="/assets/icons/logo-full-brand.svg"
             alt="logo"
             width={161}
             height={52}/>
-        </Link>
+        </Link> */}
         {navItems.map((item)=>{
           const isActive = pathname === item.url || pathname.startsWith(`${item.url}/`)
 
           return (
             <Link href={item.url} key={item.name}>
               {/* <div className="sidebar-nav-item"> */}
-              <div className={cn(`sidebar-nav-item`, {'bg-brand text-white':isActive})}>
+              <div className={cn(`sidebar-nav-item py-8`, {'bg-brand text-white':isActive})}>
                 <Image src={item.icon} alt={item.name} width={26} height={26} className="nav-icon"/>
-                <p className='h5'>{item.name}</p>
+                {/* <p className='h5 max-lg:hidden' >{item.name}</p> */}
+                <p className={cn('h5 max-lg:hidden',{'text-white':isActive})} >{item.name}</p>
               </div>
             </Link>
           )
