81af5fd214ea5d22e8a786661b417f7d2fb49b2b
Soo <parksk971031@gmail.com>
2024-12-11 18:18:01 -0500

    ongoingbug on file.actions.ts InputFile

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,11 +1,11 @@
 'use server'
-
-import { createAdminClient } from "../appwrite"
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
 import { InputFile } from "node-appwrite/file";
-import { appwriteConfig } from "../appwrite/config";
-import { constructFileUrl, getFileType, parseStringify } from "../utils";
-import { ID } from "node-appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { ID, Models, Query } from "node-appwrite";
+import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 
 interface UploadFileProps{
     file:File,
@@ -19,7 +19,7 @@ const handleError=(error:unknown, message:string)=>{
     throw error
 }
 
-export const uploadFile=async({file, ownerId, accountId, path}:UploadFileProps)=>{
+export const uploadFile = async({file, ownerId, accountId, path}:UploadFileProps) => {
     const {storage, databases}=await createAdminClient()
 
     try{
